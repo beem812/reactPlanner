4b9a500e634848bdd4967a0b11df70f802541f4a
James Beem <beem132@gmail.com>
2016-07-17 03:22:02 -0500

    added onChange function prop to LoginForm

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -5,7 +5,7 @@ import TextInput from '../common/TextInput';
  * Defining Login page container that will pass down props to
  * login components
  */
-const LoginForm = ({onLogin, user}) => {
+const LoginForm = ({onLogin, user, onChange}) => {
   return(
     <form>
       <TextInput
@@ -27,5 +27,6 @@ const LoginForm = ({onLogin, user}) => {
 LoginForm.propTypes = {
   user: React.PropTypes.object.isRequired,
   onChange: React.PropTypes.func.isRequired,
-  onLogin: React.PropTypes.func.isRequired
+  onLogin: React.PropTypes.func.isRequired,
+  onChange: React.PropTypes.func.isRequired
 };
